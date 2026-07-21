ba556d3e36f2b9694a19c277a94e6d04cbf3a963
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2026-07-21 09:09:38 -0400

    Add basic setup for Generic Hygrostat component

--- custom_components/generic_hygrostat/__init__.py
+++ custom_components/generic_hygrostat/__init__.py
@@ -1,1 +1,18 @@
-"""Generic hygrostat"""
+"""The Generic Hygrostat custom component integration."""
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers.reload import async_setup_reload_service
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "generic_hygrostat"
+PLATFORMS = ["binary_sensor"]
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+	"""Set up the Generic Hygrostat component from YAML."""
+	
+	# Registers the reload service and dynamically populates the Developer Tools UI
+	await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
+	
+	return True
