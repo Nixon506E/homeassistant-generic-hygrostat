7e5c37f58655341585d41a933cea3eb8851de385
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2026-07-21 10:32:43 -0400

    Add constants for Generic Hygrostat integration
    Defines constants for the Generic Hygrostat integration.

---
+++ custom_components/generic_hygrostat/const.py
@@ -0,0 +1,13 @@
+"""Constants for the Generic Hygrostat integration."""
+
+DOMAIN = "generic_hygrostat"
+PLATFORMS = ["binary_sensor"]
+
+# Shared Configuration Variable Strings
+CONF_SENSOR = "sensor"
+CONF_DELTA_TRIGGER = "delta_trigger"
+CONF_TARGET_OFFSET = "target_offset"
+CONF_MIN_HUMIDITY = "min_humidity"
+CONF_MIN_ON_TIME = "min_on_time"
+CONF_MAX_ON_TIME = "max_on_time"
+CONF_SAMPLE_INTERVAL = "sample_interval"
